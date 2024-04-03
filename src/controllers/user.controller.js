bb3996fd9486a22e0740e5cd5717856717c0e1fc
SudhirYadav <sudhirkyadav51@gmail.com>
2024-04-03 12:45:15 +0530

    add  register controller without testing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,67 @@
 import { asyncHandler } from "../utilies/asyncHandler.js";
+import {ApiError} from "../utilies/ApiError.js"
+import {User} from "../models/userModel.js";
+import {upuploadOnCloudinaryl} from "../utilies/cloudinary.js"
+import {ApiResponse} from "../utilies/apiResponce.js"
 
 const registerUser= asyncHandler(async (req,res)=>{
-     await res.status(201).json({
-        message:"successfully register user"
-} )
+     // get user details from frontend
+    // validation - not empty
+    // check if user already exists: username, email
+    // check for images, check for avatar
+    // upload them to cloudinary, avatar
+    // create user object - create entry in db
+    // remove password and refresh token field from response
+    // check for user creation
+    // return res
+    const {fullName, username, email, password}=req.body //req.body get data from  form or json
+    console.log("email",email, "username",username);
+//     if (fullName ==="") {
+//         throw new ApiError(400,"fullname is required")
+//     }
+        if ([fullName, email, username, password].some((fields)=> fields?.trim === "")) {
+                throw new ApiError(400,"Please fill all the fields");
+        }
+
+        //already existed user can check from "users" due users creates with mongoose
+        const existedUser=User.findOne({$or:[{email}, {username}]})
+        if (existedUser) {
+                throw new ApiError(409,"User  Already Exists! Please Login Instead.")
+        };
+
+        //files uploading with  multer "files" and express req
+        const avatarLocalPath=req.files?.avatar[0]?.path;   //multer gives path of uploaded file
+        console.log(req.files);
+        const coverImageLocalPath=req.files?.coverImage[0]?.path;
+        if (!avatarLocalPath) {
+                throw new ApiError(400,"avatar is required")
+        }
+        
+        //files upload om cloudinary
+        const avatar=await upuploadOnCloudinaryl(avatarLocalPath)
+        const coverImage=await upuploadOnCloudinaryl(coverImageLocalPath)
+        if (!avatar) {
+                throw new ApiError(400,"avatar is required")
+        }
+
+        // data upload on dataBase 
+        const user= await User.create({
+                fullName,
+                avatar: avatar.url,
+                coverImage:coverImage.url || "",
+                email,
+                username:username.toLowerCase(),
+                password
+        })
+        // check for entry in db
+        const createdUser=await User.findById(user._id).select("-password -refreshToken")
+        if (!createdUser) {
+                throw new ApiError(500,"user not in db") 
+        }
+
+        return res.status(201,).json(
+                new ApiResponse(200, createdUser, "User registered successfully")
+        )
 });
 
 export {registerUser}
--- src/utilies/cloudinary.js
+++ src/utilies/cloudinary.js
@@ -26,7 +26,4 @@ const uploadOnCloudinary = async (localFilePath) =>{
 }
 
 
-//  uploads image to the server using clodinary
-cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" }, 
-  function(error, result) {console.log(result); });
+export {uploadOnCloudinary}
